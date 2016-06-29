c37a09a3b7825c8bbbb62c444979752e10b8f2f3
Ignacio Palma <ignacio.palma.@live.com>
2016-06-29 15:07:41 -0300

    first media queries

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,7 +1,7 @@
-var formName = document.getElementById("contact_form_name");
+// var formName = document.getElementById("contact_form_name");
 
-var log = function() {
-	console.log("clickiticlick");
-}
+// var log = function() {
+// 	console.log("clickiticlick");
+// }
 
-formName.onclick = log;
+// formName.onclick = log;
