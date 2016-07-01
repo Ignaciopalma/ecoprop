51b095f4d5eac4e32ce38385ce51f045f455d622
Ignacio Palma <ignacio.palma.@live.com>
2016-07-01 15:22:33 -0300

    security authenticate admin

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,7 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_admin!, except: [ :index, :show ]
+
 
   def index
     @properties = Property.all
