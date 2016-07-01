d661aef4ecb249fe8a7506fc53401f9154bdbd89
Ignacio Palma <ignacio.palma.@live.com>
2016-07-01 20:57:30 -0300

    release 1.1

--- db/migrate/20160629152336_change_columns_default_limits.rb
+++ db/migrate/20160629152336_change_columns_default_limits.rb
@@ -1,6 +1,6 @@
 class ChangeColumnsDefaultLimits < ActiveRecord::Migration
 	def change
-		change_column :properties, :price, :integer, :limit => 10
-		change_column :properties, :common_expenses, :integer, :limit => 10	
+		change_column :properties, :price, :integer, :limit => 6
+		change_column :properties, :common_expenses, :integer, :limit => 6	
 	end
 end
