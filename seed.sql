9cf284a6c033d6d3f984c24852d836b741576c0b
Ignacio Palma <ignacio.palma@live.com>
2017-10-24 00:43:54 -0300

    fix

---
+++ seed.sql
@@ -0,0 +1,36 @@
+INSERT INTO 
+	properties (
+		created_at, 
+		updated_at, 
+		address, 
+		description, 
+		image, 
+		imagetwo,
+		imagethree,
+		imagefour,
+		imagefive
+		floors, 
+		rooms, 
+		toilets, 
+		total_area, 
+		house_area, 
+		pool, 
+		price) 
+
+	VALUES (
+		'2017-10-23 22:23:23', 
+		'2017-10-23 22:23:22', 
+		'Departamento en Vitacura', 
+		'cocina americana, terraza, vista frontal al mar, ventanas termopanel, Completamente amoblado.', 
+		'',
+		'',
+		'',
+		'',
+		'',
+		1, 
+		4, 
+		4, 
+		75, 
+		68, 1
+		, 
+		5000);
